e34b06b834ed0fa95a021eaa6eece632f21a1da3
bunturioto <Bunturioto@gmail.com>
2024-02-14 14:14:02 +0400

    add __str__ method in Perosn class

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ class Person:
         self.name = name
         self.deposit = deposit
         self.loan = loan
+
+    def __str__(self):
+        return f"{self.name} has deposit: {self.deposit} and loan: {self.loan}"
